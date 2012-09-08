dbc7f789e080b84722c4d9cef9e7fd0790bb2ad9
tokuhirom <tokuhirom@gmail.com>
2012-09-08 12:25:17 +0900

    better test case

--- test/02_loadfile.js
+++ test/02_loadfile.js
@@ -5,8 +5,8 @@ var mRuby = require('../index.js'),
 
 test(function (t) {
     var mruby = new mRuby();
-    t.equivalent(mruby.loadFile("test/rb/01_true.rb"), true);
-    t.equivalent(mruby.loadFile("test/rb/02_5963.rb"), 5963);
+    t.equivalent(mruby.loadFile(__dirname + "/rb/01_true.rb"), true);
+    t.equivalent(mruby.loadFile(__dirname + "/rb/02_5963.rb"), 5963);
     t.end();
 });
 
