28bb56355655811b6b752d5ebdd29534aea392b6
tokuhirom <tokuhirom@gmail.com>
2012-09-18 15:02:31 +0900

    oops

--- mrb-build.sh
+++ mrb-build.sh
@@ -2,5 +2,5 @@
 cd vendor/mruby/
 # make directory to output binary files.
 # mruby have a vendor/mruby/bin/.gitkeep, but npm packager ignores it and package tar ball doesn't contain it.
-mkdir -p vendor/mruby/bin/
+mkdir -p bin/
 make CFLAGS="-g -fPIC -O0"
